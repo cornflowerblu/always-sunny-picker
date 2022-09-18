f413319b118428eca79161587cbf8b69c50390eb
Roger Urich <roger@itsovertime.com>
2022-09-18 05:19:53 -0700

    init

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  const season = getSeasonOrEpisode(1, 15);
+  const episode = getSeasonOrEpisode(1, 10)
+  const characters = ['Dennis', 'Dee', 'Mac', 'Charlie', 'Frank']
+    
+  let random = Math.floor(Math.random() * characters.length);
+
+  res.render('index', { title: "Alway's Sunny Episode Picker", message: `${characters[random]} says you should watch season ${season}, episode ${episode}.` });
+});
+
+function getSeasonOrEpisode(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+module.exports = router;
