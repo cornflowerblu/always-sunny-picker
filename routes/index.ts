9e60519e3f4e6fd5b6ec9903749a58a686798604
Roger Urich <roger@itsovertime.com>
2022-09-18 14:25:07 -0700

    added iamges for the characters

--- routes/index.ts
+++ routes/index.ts
@@ -6,14 +6,22 @@ const router = express.Router();
 router.get('/', function (req: Request, res: Response, next: NextFunction) {
   const season = getSeasonOrEpisode(1, 15);
   const episode = getSeasonOrEpisode(1, 10)
-  const characters = ['Dennis', 'Dee', 'Mac', 'Charlie', 'Frank']
+  const characters = [
+    { name: 'Dennis', image: "/images/dennis.jpg" },
+    { name: 'Sweet Dee', image: "/images/dee.webp" },
+    { name: 'Mac', image: "/images/mac.webp" },
+    { name: 'Charlie', image: "/images/charlie.jpg" },
+    { name: 'Frank', image: "/images/frank.jpg" }
+  ]
 
   let random = Math.floor(Math.random() * characters.length);
 
+  const character = characters[random]
   Promise.resolve().then(() => res.render('index',
     {
       title: "Always Sunny Episode Picker",
-      message: `${characters[random]} says you should watch season ${season}, episode ${episode}.`
+      image: character.image,
+      message: `${character.name} says you should watch season ${season}, episode ${episode}.`
     })).catch(next);
 
 });
