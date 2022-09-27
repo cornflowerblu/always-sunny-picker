fed9e2afd3a2c66c76d4bf36e592a97a92314ad5
Roger Urich <rogerdurich+git@gmail.com>
2022-09-26 23:55:21 -0700

    I am the shit

--- graphql/select-episode-filters/get-single-episode.ts
+++ graphql/select-episode-filters/get-single-episode.ts
@@ -6,9 +6,12 @@ const query = gql`
   query GetOneEpisode($id: uuid!) {
     episodes_by_pk(id: $id) {
       id
+      season {
+        season_number
+      }
+      episode_number
       title
       description
-      episode_number
     }
   }`
 
@@ -20,9 +23,12 @@ type GetEpisodeResponse = {
   episodes_by_pk:
   {
     id: string,
+    season: {
+      season_number: number,
+    },
+    episode_number: number,
     title: string,
     description: string,
-    episode_number: string,
   }
 }
 
