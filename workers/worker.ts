08cf693b3893633f2201821e77e0f5cd16624753
Roger Urich <roger@itsovertime.com>
2022-10-03 14:22:41 -0700

    fix recursion and hide compose file for now

--- workers/worker.ts
+++ workers/worker.ts
@@ -4,59 +4,45 @@ import ConnectRedis from "../lib/connect-redis";
 
 require('dotenv').config();
 const WEB_SOCKET_PORT = 8000
-let subscriber;
 
-async function BackgroundSessionWork(msg?: string): Promise<void> {
-
-  const redis = ConnectRedis();
-
-  const subscriber = redis.duplicate();
-
-  const producer = subscriber.duplicate();
-
-  subscriber.on("message", async (channel, message) => {
-    const msg = message;
-    await BackgroundSessionWork(msg);
-  });
-
-
-  await subscriber.subscribe('channel', async (err, count) => {
-
-    if (err) {
-      console.error("Failed to subscribe: %s", err.message);
-    } else {
-      console.log(
-        `Subscribed successfully! This client is currently subscribed to ${count} channels.`
-      )
-    }
-  });
-
-
-  if (msg === undefined) {
-    return
-  } else {
-    try {
-      await producer.lpush('user:queue:id', msg);
-      const { id, season, episode, name } = JSON.parse(msg)
-      console.log('Items added to queue.' + JSON.stringify({
-        id: id,
-        season: season,
-        episode: episode,
-        name: name
-      }))
-    } catch (error) {
-      console.error(error);
-    }
-
+function BackgroundSessionWork(msg: string): void {
+  try {
+    producer.lpush('user:queue:id', msg);
+    const { id, season, episode, name } = JSON.parse(msg)
+    console.log('Items added to queue.' + JSON.stringify({
+      id: id,
+      season: season,
+      episode: episode,
+      name: name
+    }))
+  } catch (error) {
+    console.error(error);
   }
 }
 
 // Create & Start the WebSocket server
 const server = new WebSocket.Server({ port: WEB_SOCKET_PORT });
-Promise.resolve().then(async () => await BackgroundSessionWork());
-
-
 console.log("WebSocket server started at ws://locahost:" + WEB_SOCKET_PORT);
+
+// Redis Pub/Sub
+const redis = ConnectRedis();
+const subscriber = redis.duplicate();
+const producer = subscriber.duplicate();
+
+subscriber.subscribe('channel', async (err, count) => {
+  if (err) {
+    console.error("Failed to subscribe: %s", err.message);
+  } else {
+    console.log(
+      `Subscribed successfully! This client is currently subscribed to ${count} channels.`
+    )
+  }
+});
+
+subscriber.on("message", async (channel, message) => {
+  const msg = message;
+  BackgroundSessionWork(msg);
+});
 
 // Register event for client connection
 server.on('connection', async function connection(ws) {
