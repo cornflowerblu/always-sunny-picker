c89fd01106b3870c906c5c5442f52afb1f3cd006
Roger Urich <rogerdurich+git@gmail.com>
2022-10-04 11:09:58 -0700

    blind push cuz fuck those websockets

--- workers/worker-add-sessions.ts
+++ workers/worker-add-sessions.ts
@@ -1,24 +1,12 @@
-import ws, { WebSocket } from "ws";
+import { WebSocket } from "ws";
+import { createSessions } from "../graphql/add-sessions";
 import ConnectRedis from "../lib/connect-redis";
+import InitGraphQL from "../lib/setup-graphql";
 
 
 require('dotenv').config();
 const WEB_SOCKET_PORT = 8000
-
-async function BackgroundSessionWork(msg: string): Promise<void> {
-  try {
-    producer.lpush('user:queue:id', msg);
-    const { id, season, episode, name } = JSON.parse(msg)
-    console.log('Items added to queue.' + JSON.stringify({
-      id: id,
-      season: season,
-      episode: episode,
-      name: name
-    }))
-  } catch (error) {
-    console.error(error);
-  }
-}
+const client = InitGraphQL();
 
 // Create & Start the WebSocket server
 const server = new WebSocket.Server({ port: WEB_SOCKET_PORT });
@@ -42,7 +30,36 @@ subscriber.subscribe('channel', async (err, count) => {
 subscriber.on("message", async (channel, message) => {
   const msg = message;
   await BackgroundSessionWork(msg);
-  server.clients.forEach((client) => {
-    client.send(message, {fin: true});
-  });
+
+  const list = await GetQueue();
+    if (list.length < 50) {
+      console.log("Queue not ready to purge")
+    } else {
+      list
+        .filter(element => element.length > 0)
+        .forEach(async element => await createSessions({sessions: [JSON.parse(element)]}, client.adminRequestHeaders)
+        .then(async () => await redis.del("user:queue:id"))
+        .finally(() => console.log("Redis queue purged."))
+      )};
 });
+
+// Functions
+async function BackgroundSessionWork(msg: string): Promise<void> {
+  try {
+    producer.lpush('user:queue:id', msg);
+    const { id, season, episode, name } = JSON.parse(msg)
+    console.log('Items added to queue.' + JSON.stringify({
+      id: id,
+      season: season,
+      episode: episode,
+      name: name
+    }))
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function GetQueue(): Promise<Array<string>>{
+  const list = await redis.lrange("user:queue:id", 0, -1);
+  return list;
+}
