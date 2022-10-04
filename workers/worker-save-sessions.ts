22d104ad8703b35bf42b8a672dfbf4f146f5b8de
Roger Urich <rogerdurich+git@gmail.com>
2022-10-04 10:32:17 -0700

    finally fixed the websockets

--- workers/worker-save-sessions.ts
+++ workers/worker-save-sessions.ts
@@ -2,14 +2,51 @@ import { WebSocket } from "ws";
 import InitGraphQL from "../lib/setup-graphql";
 import { createSessions } from "../graphql/add-sessions";
 import ConnectRedis from "../lib/connect-redis";
+import net from 'net';
 
 require('dotenv').config();
 
 const client = InitGraphQL();
 
 const url = process.env.WSS_URL || 'ws://localhost:8000'
-const server = new WebSocket(url)
-console.log("WebSocket client started");
+
+let server;
+
+function init() {
+  testWebSocket();
+}
+
+function testWebSocket() {
+  server = new WebSocket(url);
+
+  server.onopen = function(evt) {
+     onOpen(evt)
+  };
+
+  server.onclose = function(evt) {
+     onClose(evt)
+  };
+
+  server.onerror = function(evt) {
+     onError(evt)
+  };
+}
+
+function onOpen(evt) {
+  console.log("CONNECTED");
+  console.log("WebSocket client ready....");
+}
+
+function onClose(evt) {
+  console.log("DISCONNECTED");
+  init();
+}
+
+function onError(evt) {
+  console.log("ERROR");
+} 
+
+init();
 
 // Redis Pub/Sub
 const redis = ConnectRedis();
@@ -19,19 +56,15 @@ async function GetQueue(): Promise<Array<string>>{
   return list;
 } 
 
-server.on('message', async () => {
-  const list = await GetQueue();
-  if (list.length < 50) {
-    console.log("Queue not ready to purge")
-  } else {
-    list
-      .filter(element => element.length > 0)
-      .forEach(async element => await createSessions({sessions: [JSON.parse(element)]}, client.adminRequestHeaders)
-      .then(async () => await redis.del("user:queue:id"))
-      .finally(() => console.log("Redis queue purged."))
-    )};
-});
-
-
-
-  
+  server.on('message', async () => {
+    const list = await GetQueue();
+    if (list.length < 50) {
+      console.log("Queue not ready to purge")
+    } else {
+      list
+        .filter(element => element.length > 0)
+        .forEach(async element => await createSessions({sessions: [JSON.parse(element)]}, client.adminRequestHeaders)
+        .then(async () => await redis.del("user:queue:id"))
+        .finally(() => console.log("Redis queue purged."))
+      )};
+  });
