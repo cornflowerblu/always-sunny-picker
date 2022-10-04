dffc3b232c93c8dd7f0994412508190722719e9d
Roger <53064994+cornflowerblu@users.noreply.github.com>
2022-10-04 14:32:46 -0700

    Redis generic (#3)
    * initial pass
    
    * generic redis

---
+++ lib/redis.ts
@@ -0,0 +1,42 @@
+import Redis from "ioredis"
+import invariant from "tiny-invariant"
+
+export function ConnectRedis(): Redis {
+  const REDIS_URL = process.env.REDIS_URL;
+  invariant(REDIS_URL, "REDIS URL NOT SET!");
+  const redis = new Redis(REDIS_URL);
+  return redis;
+};
+
+export function PubSub(channel: string): {subscriber: Redis, producer: Redis} {
+  const redis = ConnectRedis();
+  const subscriber = redis.duplicate();
+  const producer = redis.duplicate();
+
+  subscriber.subscribe(channel, async (err, count) => {
+    if (err) {
+      console.error("Failed to subscribe: %s", err.message);
+    } else {
+      console.log(
+        `Subscribed successfully! This client is currently subscribed to ${count} channels.`
+      )
+    }
+  });
+
+  return {subscriber, producer}
+}
+
+export async function PushToQueue(producer: Redis, key: string, {params} ): Promise<void> {
+  try {
+    await producer.lpop(key, 1);
+    await producer.lpush(key, JSON.stringify(params));
+    console.log('Items added to queue: ' + JSON.stringify(params))
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function GetQueue(key: string, redis: Redis): Promise<Array<string>>{
+  const list = await redis.lrange(key, 0, -1);
+  return list;
+}
--- workers/worker-add-sessions.ts
+++ workers/worker-add-sessions.ts
@@ -1,5 +1,5 @@
 import { createSessions } from "../graphql/add-sessions";
-import ConnectRedis from "../lib/connect-redis";
+import {ConnectRedis, PubSub, PushToQueue, GetQueue} from "../lib/redis";
 import InitGraphQL from "../lib/setup-graphql";
 
 // ENV
@@ -10,52 +10,18 @@ const client = InitGraphQL();
 
 // Redis Pub/Sub
 const redis = ConnectRedis();
-const subscriber = redis.duplicate();
-const producer = subscriber.duplicate();
-
-subscriber.subscribe('channel', async (err, count) => {
-  if (err) {
-    console.error("Failed to subscribe: %s", err.message);
-  } else {
-    console.log(
-      `Subscribed successfully! This client is currently subscribed to ${count} channels.`
-    )
-  }
-});
+const {subscriber, producer} = PubSub('channel');
+const key = "user:queue:id"
 
 subscriber.on("message", async (channel, message) => {
-  const msg = message;
-  await BackgroundSessionWork(msg);
-
-  const list = await GetQueue();
-    if (list.length < 50) {
-      return;
-    } else {
-      list
-        .filter(element => element.length > 0)
-        .forEach(async element => await createSessions({sessions: [JSON.parse(element)]}, client.adminRequestHeaders)
-        .then(async () => await redis.del("user:queue:id"))
-        .finally(() => console.log("Redis queue purged."))
-      )};
-});
-
-// Functions
-async function BackgroundSessionWork(msg: string): Promise<void> {
-  try {
-    producer.lpush('user:queue:id', msg);
-    const { id, season, episode, name } = JSON.parse(msg)
-    console.log('Items added to queue.' + JSON.stringify({
-      id: id,
-      season: season,
-      episode: episode,
-      name: name
-    }))
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-async function GetQueue(): Promise<Array<string>>{
-  const list = await redis.lrange("user:queue:id", 0, -1);
-  return list;
-}
+  const { id, season, episode, name } = JSON.parse(message)
+  await PushToQueue(producer, key, {params: {id, season, episode, name}})
+  const list = await GetQueue(key, redis);
+    
+  list.length < 50 ? list : list
+    .filter(element => element.length > 0)
+    .forEach(async element => 
+      await createSessions({sessions: [JSON.parse(element)]}, client.adminRequestHeaders)
+    .then(async () => await redis.del(key)),        
+    console.log("Redis queue purged.")
+)});
