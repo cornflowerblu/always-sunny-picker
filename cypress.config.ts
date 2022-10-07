1eed7d342403cc90e7fae053d721b83fb901072e
Roger <rogerdurich+git@gmail.com>
2022-10-07 09:59:29 -0700

    update cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    "viewportHeight": 1200,
+    "viewportWidth": 1600,
     setupNodeEvents(on, config) {
       //stufff
     },
