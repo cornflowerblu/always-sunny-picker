7cf13822b7763f837c6138d9b4b304089127e1a9
Roger <rogerdurich+git@gmail.com>
2022-10-12 10:56:19 -0700

    this better be it

--- hasura/config/migrations/shuffle/1665586930205_auth_users_auth_sessions/up.sql
+++ hasura/config/migrations/shuffle/1665586930205_auth_users_auth_sessions/up.sql
@@ -1,26 +1,27 @@
-create table auth_users
+CREATE TABLE public.auth_users
 (
-    id       uuid default gen_random_uuid() not null PRIMARY KEY,
+    id       uuid default gen_random_uuid() not null,
     email    text                           not null,
     password text                           not null
 );
 
-create unique index auth_users_email_key
-    on auth_users (email);
+ALTER TABLE ONLY public.auth_users
+    ADD CONSTRAINT auth_users_pkey PRIMARY KEY (id);
+    CREATE UNIQUE INDEX auth_users_email_key on auth_users (email);
 
 
-create table auth_sessions
+CREATE TABLE public.auth_sessions
 (
-    id        uuid default gen_random_uuid() not null PRIMARY KEY,
+    id        uuid default gen_random_uuid() not null,
     token     text                           not null,
     user_id   uuid                           not null,
     enc_token text                           not null,
     time      timestamptz                    not null
 );
 
-create unique index enc_token
-    on auth_sessions (enc_token);
+ALTER TABLE ONLY public.auth_sessions
+    ADD CONSTRAINT auth_sessions_pk PRIMARY KEY (id);
+    CREATE UNIQUE INDEX enc_token on auth_sessions (enc_token);
 
-[CONSTRAINT fk_auth_user]
-    FOREIGN KEY (user_id)
-    REFERENCES auth_users(id);
+ALTER TABLE ONLY public.auth_sessions
+    ADD CONSTRAINT fk_auth_user FOREIGN KEY (user_id) REFERENCES public.auth_users(id);
