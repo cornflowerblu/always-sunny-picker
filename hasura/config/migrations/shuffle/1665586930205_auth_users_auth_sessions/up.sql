6d5496fe28b205a121f5b1483561bb6eb118c6f4
Roger <rogerdurich+git@gmail.com>
2022-10-12 10:26:20 -0700

    syntax errors in migrations

--- hasura/config/migrations/shuffle/1665586930205_auth_users_auth_sessions/up.sql
+++ hasura/config/migrations/shuffle/1665586930205_auth_users_auth_sessions/up.sql
@@ -18,11 +18,8 @@ create table auth_sessions
     token     text                           not null,
     user_id   uuid                           not null,
     enc_token text                           not null,
-    time      timestamp with timezone        not null
+    time      timestamptz                    not null
 );
-
-create unique index auth_sessions_pkey
-    on auth_sessions (id);
 
 create unique index enc_token
     on auth_sessions (enc_token);
