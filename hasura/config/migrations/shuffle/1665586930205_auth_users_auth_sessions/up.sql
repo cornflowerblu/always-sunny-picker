ce6464ccbcdc3ce3c993456d7bb720a3ed4899b8
Roger <rogerdurich+git@gmail.com>
2022-10-12 08:03:34 -0700

    DONT LOSE THIS
    in case your db doesn't come back after restart

---
+++ hasura/config/migrations/shuffle/1665586930205_auth_users_auth_sessions/up.sql
@@ -0,0 +1,28 @@
+create table auth_users
+(
+    id       uuid default gen_random_uuid() not null PRIMARY KEY,
+    email    text                           not null,
+    password text                           not null
+);
+
+create unique index auth_users_pkey
+    on auth_users (id);
+
+create unique index auth_users_email_key
+    on auth_users (email);
+
+
+create table auth_sessions
+(
+    id        uuid default gen_random_uuid() not null PRIMARY KEY,
+    token     text                           not null,
+    user_id   uuid                           not null,
+    enc_token text                           not null,
+    date      bigint                         not null
+);
+
+create unique index auth_sessions_pkey
+    on auth_sessions (id);
+
+create unique index enc_token
+    on auth_sessions (enc_token);
